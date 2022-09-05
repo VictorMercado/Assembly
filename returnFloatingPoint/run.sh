5a90206f13b3c33b0af5725edf509de4dfbc36fa
Victor Mercado <victormercado@outlook.com>
2022-09-04 18:06:18 -0700

    fixed bash file

--- returnFloatingPoint/run.sh
+++ returnFloatingPoint/run.sh
@@ -12,11 +12,14 @@ echo "Bash: This is program <Demonstrate Numeric IO>"
 echo "Bash: Assemble the module floatin_point_processor.asm"
 nasm -f elf64 -l floating_point_processor.lis -o floating_point_processor.o floating_point_processor.asm
 
+echo "Bash: Compile the C++ module islfoat.cpp"
+g++ -c -m64 -Wall -o isfloat.o isfloat.cpp -fno-pie -no-pie -std=c++17
+
 echo "Bash: Compile the C++ module main.cpp"
-g++ -c -m64 -Wall -o main.o main.cpp isfloat.cpp -fno-pie -no-pie -std=c++17
+g++ -c -m64 -Wall -o main.o main.cpp -fno-pie -no-pie -std=c++17
 
 echo "Bash: Link the two object files already created"
-g++ -m64 -o floating_point_processor main.o floating_point_processor.o -fno-pie -no-pie -std=c++17
+g++ -m64 -o floating_point_processor main.o floating_point_processor.o isfloat.o -fno-pie -no-pie -std=c++17
 
 echo "Bash: Run the program Basic Float Operations"
 ./floating_point_processor
