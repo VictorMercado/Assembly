120a3da03eceb22d93843f39a762b47378944092
Victor Mercado <victormercado@outlook.com>
2022-09-04 22:47:56 -0700

    finishing touches

--- returnFloatingPoint/main.cpp
+++ returnFloatingPoint/main.cpp
@@ -15,14 +15,15 @@ extern "C" double floating_point_processor();
 // this is the driver function for the program
 int main()
 {
-
+    const string START_MOD = "\033[1;31m";
+    const string END_MOD = "\033[0m\n";
     double return_code = -1;
-    cout << "\033[1;31m Welcome to Floating Points Numbers programmed by Victor Mercado. \033[0m\n";
-    cout << "C++: You will be entering 2 Floating Point numbers and its my job to see if you did just that." << endl;
-    
+    cout << START_MOD << "Welcome to Floating Points Numbers programmed by Victor Mercado." << END_MOD;
+    cout << START_MOD << "You will be entering 2 Floating Point numbers and its my job to see if you did just that." << END_MOD;
+
     //assmebly function call to process the floating point numbers
     return_code = floating_point_processor();
-    cout <<"C++: The driver received this float number: " <<  return_code << ".  The driver will now exit.  GoodBye." << endl;
-    cout << "C++: The driver module will return integer 0 to the operating system." << endl;
+    cout << START_MOD << "The driver received this float number: " << return_code << ".  The driver will now exit.  GoodBye." << END_MOD;
+    cout << START_MOD << "The driver module will return integer 0 to the operating system." << END_MOD;
     return 0;
 }
