ea4c2a9ec831792f681b30e9ac789df7412c8982
Victor Mercado <victormercado@outlook.com>
2022-09-04 22:02:37 -0700

    final console prints

--- returnFloatingPoint/floating_point_processor.asm
+++ returnFloatingPoint/floating_point_processor.asm
@@ -149,11 +149,10 @@ movsd xmm0, xmm14
 pop rax
 
 
-; mov rax, 0
-; mov rdi, string_form
-; mov rsi, return
-; mov rdx, goodbye
-; call printf
+mov rax, 0
+mov rdi, string_form
+mov rsi, return
+call printf
 
 
 ; read input
