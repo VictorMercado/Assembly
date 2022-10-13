ed9c28f48374ca8202c44f381e010859dfc6581f
Victor Mercado <victormercado@outlook.com>
2022-10-12 18:49:43 -0700

    shell test

--- assignment3/main.c
+++ assignment3/main.c
@@ -5,7 +5,7 @@ extern void manager();
 int main() 
 {
     // double number = 0;
-    printf("\e[1;31m Welcome to ArrayIO \n");
+    printf("\e[1;31mWelcome to ArrayIO \n");
     manager();
     // scanf("%lf", &number);
     // printf("%lf", number);
