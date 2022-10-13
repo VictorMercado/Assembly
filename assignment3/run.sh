6b24f272d796a86c64e903bc08946947a81b13a0
Victor Mercado <victormercado@outlook.com>
2022-10-12 18:43:17 -0700

    first test

--- assignment3/run.sh
+++ assignment3/run.sh
@@ -5,17 +5,17 @@ echo "Bash: This is program <Demonstrate Numeric IO>"
 
 echo "Bash: Assemble the asm files"
 nasm -f elf64 -l manager.lis -o manager.o manager.asm
-nasm -f elf64 -l sum.lis -o sum.o sum.asm
-nasm -f elf64 -l input_array.lis -o input_array.o input_array.asm
+# nasm -f elf64 -l sum.lis -o sum.o sum.asm
+# nasm -f elf64 -l input_array.lis -o input_array.o input_array.asm
 
-# echo "Bash: Compile the C driver main.c"
+echo "Bash: Compile the C driver main.c"
 gcc -c -m64 -Wall -o main.o main.c -fno-pie -no-pie -std=c++17
 
 echo "Bash: Compile the C++ module pythagoras.cpp"
-g++ -c -m64 -Wall -o display_Array.o display_Array.cpp -fno-pie -no-pie -std=c++17
+# g++ -c -m64 -Wall -o display_Array.o display_Array.cpp -fno-pie -no-pie -std=c++17
 
 echo "Bash: Link the two object files already created"
-g++ -m64 -o arrayIO manager.o sum.o input_array.o display_Array.o main.o -fno-pie -no-pie -std=c++17
+g++ -m64 -o arrayIO manager.o main.o -fno-pie -no-pie -std=c++17      # sum.o input_array.o display_Array.o
 
 echo "Bash: Run the program Basic Float Operations"
 ./arrayIO
