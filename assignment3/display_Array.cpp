10d3239094f15467ec6e50044176bfb58c175a4d
Victor Mercado <victormercado@outlook.com>
2022-10-12 22:12:14 -0700

    testing

--- assignment3/display_Array.cpp
+++ assignment3/display_Array.cpp
@@ -5,7 +5,7 @@ extern "C" void display_Array(int arr[], int size);
 
 void display_Array(int arr[], int size) {
     for (int i = 0; i < size; i++) {
-        printf("%d ", arr);
+        printf("%d", arr[i]);
     }
     printf("\n");
 }
