c71b1275f9ed98ef20f39a44f351b516c33fd42d
Victor Mercado <victormercado@outlook.com>
2022-10-12 23:48:14 -0700

    testing

--- assignment3/sum.asm
+++ assignment3/sum.asm
@@ -11,17 +11,19 @@ section .text
 ; rdi has 1 param - pointer to array
 ; rsi has 2 param - size of array
 ; sum up all elements of array and return result in rax
+
+sum:
 mov r12, 0
 mov rax, 0
 mov r13, rsi
-dec r13
-sum:
-cmp r12, r13
-je done
 
-add rax, [rdi + r12 * 4]
-inc r12
-jmp sum
+loop:
+    cmp r12, r13
+    je done
+
+    add rax, [rdi + r12 * 4]
+    inc r12
+jmp loop
 
 
 
