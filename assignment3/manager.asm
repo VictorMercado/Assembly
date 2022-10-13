496446dfd004ade463612d3670ffee3fd7b95742
Victor Mercado <victormercado@outlook.com>
2022-10-12 20:20:36 -0700

    testing

--- assignment3/manager.asm
+++ assignment3/manager.asm
@@ -11,7 +11,7 @@ welcome_message db "Hello, this program will add your floats!", 10, 0
 show_numbers db "Your numbers are: ", 10, 0
 sum_message db "The sum of your numbers is: ", 10, 0
 exit_message db "This program will now exit back to the driver.", 10, 0
-
+aCoolNumber dd 150
 
 string_form db "%s", 0
 ; d is 32-bit
