7a9ba341e2ad4658473631217a03d15d99d86757
Victor Mercado <victormercado@outlook.com>
2022-10-12 23:11:46 -0700

    testing

--- assignment3/sum.asm
+++ assignment3/sum.asm
@@ -4,13 +4,22 @@ extern printf
 global sum
 
 section .data
-digit_form db "This is how many array items there is: %d", 0
+digit_form db "This is how many array items there is: %d \n", 0
+start_code db "\e[32m", 0
+end_code db "\e[0m", 0
 
 section .text
 
 ; sum up all elements of array
 sum:
+push rsi
+mov rdi, start_code
+call printf
 
 mov rax, 0
 mov rdi, digit_form
+pop rsi
+call printf
+
+mov rdi, end_code
 call printf
