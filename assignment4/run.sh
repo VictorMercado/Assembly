b647b800c0ed358a69576dd084ad630536e42a72
Victor Mercado <victormercado@outlook.com>
2022-10-22 20:33:48 -0700

    fixed stuff

--- assignment4/run.sh
+++ assignment4/run.sh
@@ -1,7 +1,7 @@
 rm *.o
 rm *.out
 
-echo "Bash: This is program <Demonstrate Numeric IO>"
+echo "Bash: This is program <Cosine Calc>"
 
 echo "Bash: Assemble the asm files"
 nasm -f elf64 -l consineCalc.lis -o consineCalc.o consineCalc.asm
@@ -10,7 +10,7 @@ nasm -f elf64 -l consineCalc.lis -o consineCalc.o consineCalc.asm
 # nasm -f elf64 -l ftoa.lis -o ftoa.o ftoa.asm
 
 
-ld -m elf_x86_64 -o consineCalc consineCalc.o #cosine.o atof.o ftoa.o 
+ld -g -o consineCalc consineCalc.o #cosine.o atof.o ftoa.o 
 
 ./consineCalc
 
