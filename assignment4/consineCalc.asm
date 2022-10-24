dcff1730f4ed3a80ab6a43b8f79090da041d772b
Victor Mercado <victormercado@outlook.com>
2022-10-24 11:35:09 -0700

    fixed stuff]

--- assignment4/consineCalc.asm
+++ assignment4/consineCalc.asm
@@ -5,7 +5,7 @@ section .data
 welcomeMsg db "Welcome to Cosine Calc!", 10, 0
 lenWelcomeMsg equ $-welcomeMsg
 
-aNumToPrint db 12345, 10, 0
+aNumToPrint dw 12345, 10, 0
 lenANumToPrint equ $-aNumToPrint
 
 timeNow db "This is the time now in ticks: ", 0
