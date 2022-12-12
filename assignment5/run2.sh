08a508c845a7c8546be15f1e8fd5e917647e415a
Victor Mercado <victormercado@outlook.com>
2022-12-11 21:20:52 -0800

    fixed issue

--- assignment5/run2.sh
+++ assignment5/run2.sh
@@ -3,17 +3,23 @@
 rm *.o
 rm *.out
 
-echo "Bash: This is program <Cosine Calc>"
+echo "Bash: This is program <Timed Array Sort>"
 
 echo "Bash: Assemble the asm files"
-nasm -f elf64 -l cosineCalc.lis -o cosineCalc.o cosineCalc.asm -gdwarf
-nasm -f elf64 -l ltoa.lis -o ltoa.o ltoa.asm -gdwarf
-nasm -f elf64 -l cosine.lis -o cosine.o cosine.asm
-nasm -f elf64 -l atof.lis -o atof.o atof.asm -gdwarf
-nasm -f elf64 -l itoa.lis -o itoa.o itoa.asm -gdwarf
-nasm -f elf64 -l ftoa.lis -o ftoa.o ftoa.asm -gdwarf
+nasm -f elf64 -l manager.lis -o manager.o manager.asm -gdwarf
+# nasm -f elf64 -l cosineCalc.lis -o cosineCalc.o cosineCalc.asm -gdwarf
+# nasm -f elf64 -l ltoa.lis -o ltoa.o ltoa.asm -gdwarf
+# nasm -f elf64 -l cosine.lis -o cosine.o cosine.asm
+# nasm -f elf64 -l atof.lis -o atof.o atof.asm -gdwarf
+# nasm -f elf64 -l itoa.lis -o itoa.o itoa.asm -gdwarf
+# nasm -f elf64 -l ftoa.lis -o ftoa.o ftoa.asm -gdwarf
 
 
-ld -o cosineCalc cosineCalc.o ltoa.o atof.o ftoa.o itoa.o cosine.o  
+# ld -o cosineCalc cosineCalc.o ltoa.o atof.o ftoa.o itoa.o cosine.o 
+gcc -c -m64 -Wall -o sort.o sort.c -fno-pie -no-pie -std=c11
 
-gdb ./cosineCalc
+g++ -c -m64 -o driver.o driver.cpp -fno-pie -no-pie -std=c++17
+
+g++ -m64 -o TimedArraySort driver.o sort.o manager.o -fno-pie -no-pie -std=c++17
+
+./TimedArraySort
