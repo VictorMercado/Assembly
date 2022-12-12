c42f6e5951c06d077ca51d0268fa5bf5f3d507a4
Victor Mercado <victormercado@outlook.com>
2022-12-12 11:25:08 -0800

    fixed issue

--- assignment5/run.sh
+++ assignment5/run.sh
@@ -10,7 +10,7 @@ echo "Bash: Assemble the asm files"
 nasm -f elf64 -o manager.o manager.asm
 nasm -f elf64 -o get_frequency.o get_frequency.asm
 # nasm -f elf64 -l cosine.lis -o cosine.o cosine.asm
-# nasm -f elf64 -l atof.lis -o atof.o atof.asm
+nasm -f elf64 -l atof.lis -o atof.o atof.asm
 # nasm -f elf64 -l itoa.lis -o itoa.o itoa.asm
 # nasm -f elf64 -l ftoa.lis -o ftoa.o ftoa.asm
 
@@ -20,7 +20,7 @@ gcc -c -m64 -Wall -o sort.o sort.c -fno-pie -no-pie -std=c11
 
 g++ -c -m64 -o driver.o driver.cpp -fno-pie -no-pie -std=c++17
 
-g++ -m64 -o TimedArraySort driver.o sort.o manager.o get_frequency.o -fno-pie -no-pie -std=c++17
+g++ -m64 -o TimedArraySort driver.o sort.o manager.o get_frequency.o atof.o -fno-pie -no-pie -std=c++17
 
 ./TimedArraySort
 
--- assignment5/run2.sh
+++ assignment5/run2.sh
@@ -10,7 +10,7 @@ nasm -f elf64 -l manager.lis -o manager.o manager.asm -gdwarf
 nasm -f elf64 -o get_frequency.o get_frequency.asm -gdwarf
 # nasm -f elf64 -l ltoa.lis -o ltoa.o ltoa.asm -gdwarf
 # nasm -f elf64 -l cosine.lis -o cosine.o cosine.asm
-# nasm -f elf64 -l atof.lis -o atof.o atof.asm -gdwarf
+nasm -f elf64 -l atof.lis -o atof.o atof.asm -gdwarf
 # nasm -f elf64 -l itoa.lis -o itoa.o itoa.asm -gdwarf
 # nasm -f elf64 -l ftoa.lis -o ftoa.o ftoa.asm -gdwarf
 
@@ -20,6 +20,6 @@ gcc -c -m64 -Wall -o sort.o sort.c -fno-pie -no-pie -std=c11
 
 g++ -c -m64 -o driver.o driver.cpp -fno-pie -no-pie -std=c++17
 
-g++ -m64 -o TimedArraySort driver.o sort.o manager.o -fno-pie -no-pie -std=c++17
+g++ -m64 -o TimedArraySort driver.o sort.o manager.o get_frequency.o atof.o -fno-pie -no-pie -std=c++17
 
 gdb ./TimedArraySort
