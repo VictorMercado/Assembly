fe1a16192581ad41fd22f70e91ee5f02182e342b
Victor Mercado <victormercado@outlook.com>
2022-12-11 21:36:32 -0800

    fixed issue

--- assignment5/manager.asm
+++ assignment5/manager.asm
@@ -18,8 +18,8 @@ displayArrayMessage2Len equ $ - displayArrayMessage2
 displayArrayMessage3 db "Here are the third list values of the sorted array ", 0
 displayArrayMessage3Len equ $ - displayArrayMessage3
 
-integer_Format db "%d"
-int_print_Format db "%d\n"
+integer_Format db "%lld" 
+int_print_Format db "%lld\n"
 
 timed_massage db "The time taken to sort the array is: ", 0
 timed_massageLen equ $ - timed_massage
