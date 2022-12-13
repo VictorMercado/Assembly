7e12006e6ee215e894c5d75e691131a54788e88f
Victor Mercado <victormercado@outlook.com>
2022-12-13 09:32:06 -0800

    fixed issue

--- final/run.sh
+++ final/run.sh
@@ -1,25 +1,14 @@
 rm *.o
 rm *.out
-
 echo "Assemble manager.asm"
 nasm -f elf64  -o manager.o manager.asm
-
 echo "Assemble getResistance.asm"
 nasm -f elf64 -o getResistance.o getResistance.asm
-
 echo "Assemble loadArray.asm"
 nasm -f elf64 -o loadArray.o loadArray.asm
-
-
 echo "compile driver.cpp using g++ compiler"
 gcc -c -Wall -m64 -o driver.o driver.cpp -fno-pie -no-pie -std=c++17
-
-# getResistance.o loadArray.o
-
 echo "Link object files using the g++ Linker"
 g++ -m64 -o finalElectricity.out manager.o driver.o  -fno-pie -no-pie -std=c++17
-
 echo "Run the Electricity Program:"
 ./finalElectricity.out
-
-echo "Script file terminated."
